62b185a2ab1528567e4927273816b443277492ea
ruifernandees <dev.ruifernandes@gmail.com>
2022-05-31 16:35:13 -0300

    test: user card props

--- src/components/user-card/user-card.spec.tsx
+++ src/components/user-card/user-card.spec.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import {render} from '@testing-library/react-native';
-import {UserCardComponent} from '.';
+import { render } from '@testing-library/react-native';
+import { UserCardComponent } from '.';
 
 describe('UserCardComponent', () => {
+  const imageUri = 'https://github.com/ruifernandees.png';
   test('Should render correctly', () => {
-    render(
+    const { getByText, getByTestId } = render(
       <UserCardComponent
-        userImage={{uri: 'https://github.com/ruifernandees.png'}}
+        userImage={{ uri: imageUri }}
         userName="Rui Fernandes"
         userRole="Developer"
       />,
     );
+    const image = getByTestId('profile-image');
+    expect(image.props.source.uri).toBe(imageUri);
+    expect(() => getByText('Rui Fernandes')).not.toThrow();
+    expect(() => getByText('Developer')).not.toThrow();
   });
 });
